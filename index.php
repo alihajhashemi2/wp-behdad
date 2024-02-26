5c50ede0ad16300c7d387ae6c04f8fbb3f005eb0
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2024-02-26 19:02:48 +0330

    Fix header duplicate bug

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 // Header
 get_header();
 
-// Main Header
-get_template_part("/templates/wp-behdad-header");
-
-// Show Case
-get_template_part("/templates/wp-behdad-showcase");
-
-// Slider
-get_template_part("/templates/wp-behdad-slider");
-
 // Store
 get_template_part("/templates/wp-behdad-store");
 
