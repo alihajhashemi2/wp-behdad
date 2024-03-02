2fcebb4b0b8d498fb274f6dec9bdea1b615fc1a8
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2024-03-02 10:25:31 +0330

    Add sidebar for products section

--- functions.php
+++ functions.php
@@ -45,9 +45,24 @@ function theme_setup()
 
     add_theme_support("custom-logo");
 
-    add_theme_support("side-bar");
-
     add_theme_support("woocommerce");
 }
 
 add_action('after_setup_theme', "theme_setup");
+
+/**
+ * Add a sidebar.
+ */
+function wpdocs_theme_slug_widgets_init()
+{
+    register_sidebar(array(
+        'name'          => __('Main Sidebar', 'textdomain'),
+        'id'            => 'sidebar-1',
+        'description'   => __('Widgets in this area will be shown on all posts and pages.', 'textdomain'),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ));
+}
+add_action('widgets_init', 'wpdocs_theme_slug_widgets_init');
