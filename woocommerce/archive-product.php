5f066f546393490d4b68960ae6e0bdec524a3355
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2024-03-24 13:03:44 +0330

    remove store section title

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive
  *
@@ -15,9 +16,9 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-get_header( 'shop' );
+get_header('shop');
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -26,14 +27,10 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+do_action('woocommerce_before_main_content');
 
 ?>
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
-
 	<?php
 	/**
 	 * Hook: woocommerce_archive_description.
@@ -41,11 +38,11 @@ do_action( 'woocommerce_before_main_content' );
 	 * @hooked woocommerce_taxonomy_archive_description - 10
 	 * @hooked woocommerce_product_archive_description - 10
 	 */
-	do_action( 'woocommerce_archive_description' );
+	do_action('woocommerce_archive_description');
 	?>
 </header>
 <?php
-if ( woocommerce_product_loop() ) {
+if (woocommerce_product_loop()) {
 
 	/**
 	 * Hook: woocommerce_before_shop_loop.
@@ -54,20 +51,20 @@ if ( woocommerce_product_loop() ) {
 	 * @hooked woocommerce_result_count - 20
 	 * @hooked woocommerce_catalog_ordering - 30
 	 */
-	do_action( 'woocommerce_before_shop_loop' );
+	do_action('woocommerce_before_shop_loop');
 
 	woocommerce_product_loop_start();
 
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
+	if (wc_get_loop_prop('total')) {
+		while (have_posts()) {
 			the_post();
 
 			/**
 			 * Hook: woocommerce_shop_loop.
 			 */
-			do_action( 'woocommerce_shop_loop' );
+			do_action('woocommerce_shop_loop');
 
-			wc_get_template_part( 'content', 'product' );
+			wc_get_template_part('content', 'product');
 		}
 	}
 
@@ -78,14 +75,14 @@ if ( woocommerce_product_loop() ) {
 	 *
 	 * @hooked woocommerce_pagination - 10
 	 */
-	do_action( 'woocommerce_after_shop_loop' );
+	do_action('woocommerce_after_shop_loop');
 } else {
 	/**
 	 * Hook: woocommerce_no_products_found.
 	 *
 	 * @hooked wc_no_products_found - 10
 	 */
-	do_action( 'woocommerce_no_products_found' );
+	do_action('woocommerce_no_products_found');
 }
 
 /**
@@ -93,13 +90,13 @@ if ( woocommerce_product_loop() ) {
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
-do_action( 'woocommerce_after_main_content' );
+do_action('woocommerce_after_main_content');
 
 /**
  * Hook: woocommerce_sidebar.
  *
  * @hooked woocommerce_get_sidebar - 10
  */
-do_action( 'woocommerce_sidebar' );
+do_action('woocommerce_sidebar');
 
-get_footer( 'shop' );
+get_footer('shop');
