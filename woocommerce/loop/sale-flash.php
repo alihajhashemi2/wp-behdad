40b9218b3b69a7656c6f458941e705e5d1a6e8f0
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2024-04-07 08:15:50 +0330

    remove onsale badge on main store page products

--- woocommerce/loop/sale-flash.php
+++ woocommerce/loop/sale-flash.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Product loop sale flash
  *
@@ -15,18 +16,5 @@
  * @version     1.6.4
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
-
-global $post, $product;
-
-?>
-<?php if ( $product->is_on_sale() ) : ?>
-
-	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $post, $product ); ?>
-
-	<?php
-endif;
 
 /* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
